9cfa77900eacb4c84add50723ee223ea02d7cb3e
Ariel <arielbs1991@gmail.com>
2020-07-09 11:50:43 -0700

    preparing to pull Matt's changes then push

--- server.js
+++ server.js
@@ -11,19 +11,24 @@ const guestsArray = [];
 const waitList = [];
 
 app.get("/", function (req, res) {
-    res.send("Welcome to the MAK Hot Restaurant!");
+    // res.send("Welcome to the MAK Hot Restaurant!");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 app.get("/tables", function (req, res) {
-    res.send("Current Reservations and Waiting List");
+    // res.send("Current Reservations and Waiting List");
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 app.get("/reservation", function (req, res) {
-    res.send("Make Your Reservation");
+    // res.send("Make Your Reservation");
+    res.sendFile(path.join(__dirname, "reservation.html"));
 });
 app.get("/api/tables", function (req, res) {
-    res.send("");
+    // res.send("this is where we'll get our table info");
+    res.json(guestsArray);
 });
 app.get("/api/waitlist", function (req, res) {
-    res.send("");
+    // res.send("this will have our waitlist info");
+    res.json(waitList);
 });
 
 app.post("/api/guests", function (req, res) {
